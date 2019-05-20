04a8da5a972a71c1149745b30c1120fb835a9e6f
imanezouhour11 <50811693+imanezouhour11@users.noreply.github.com>
2019-05-20 02:40:38 +0200

    Add files via upload

---
+++ users.js
@@ -0,0 +1,26 @@
+let users = [{
+    name: 'user1',
+    bio: 'bio biob bkldjha',
+    img_url: 'la.jpg'
+},
+{
+    name: 'user2',
+    bio: 'bio biob bkldjha',
+    img_url: 'naruto.jpg'
+},
+{
+    name: 'user3',
+    bio: 'bio biob bkldjha',
+    img_url: 'naruto.jpg'
+},
+{
+    name: 'user4',
+    bio: 'bio biob bkldjha',
+    img_url: 'naruto1.jpg'
+},
+{
+    name: 'user5',
+    bio: 'bio biob bkldjha',
+    img_url: 'ny.jpg'
+}
+];
